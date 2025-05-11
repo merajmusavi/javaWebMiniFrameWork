b82039a7811558c06fa8ab42581d725cfda8e005
merajmusavi <merajmusavi1243m@gmail.com>
2025-05-11 13:07:12 +0330

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000),0);
+        httpServer.start();
+        System.out.println("server started on port 8000");
+    }
+}
